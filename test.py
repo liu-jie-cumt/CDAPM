807150f531ac0bb9fc1cee6e31ff16847bcc13b0
zhushaojie <1208162499@qq.com>
2021-04-12 22:13:51 +0800

    rebuild

--- test.py
+++
@@ -1,9 +0,0 @@
-from geo_data_decoder import *
-from eval_tools import *
-import config
-from read_features import *
-
-
-user_feature_sequence, place_index, seg_max_record, center_location_list, useful_vec = geo_data_clean_fs()
-train_X, train_Y, train_evl, vali_X, vali_Y, vali_evl, user_dim, word_vec, word_index \
-            = geo_dataset_train_test_text(user_feature_sequence, useful_vec, seg_max_record)
